ac09415c6a33a0f893ce365d9d93e7531c2356ab
sakshamg567 <saksham060306@gmail.com>
2025-01-08 19:36:47 +0530

    started work on frontend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const dotenv = require("dotenv");
-
+const path = require("path");
 
 
 const router = require("./routes/app.router")
@@ -9,9 +9,12 @@ const router = require("./routes/app.router")
 dotenv.config();
 const app = express();
 app.use(express.json()); // Middleware to parse JSON bodies
-
+app.use(express.static(path.join(__dirname, 'dist')))
 const PORT = process.env.PORT || 3000
 
 app.use("/api/analyze", router );
+app.get("*", (req,res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
 
 app.listen(PORT, () => console.log(`Server started on port : ${PORT}` ));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+export default function App() {
+
+  return (
+    <>
+      <div className="bg-gradient-to-br from-blue-200 via-blue-300  to-blue-200 min-h-screen flex items-center justify-center flex-col">
+        <div className="container flex items-center justify-center min-h-screen flex-col">
+          <span className="font-extrabold text-5xl max-w-2xl text-center relative -top-24 font-poppin ">
+            Unlock the power of Data, Drive smarter Social Engagement
+          </span>
+          <button className="self-center bg-blue-500 text-white min-w-32 min-h-11 rounded-full   hover:scale-125 hover:border-2 border-white absolute mt-32 transition-all duration-300 ease-in-out">Try Now !</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
