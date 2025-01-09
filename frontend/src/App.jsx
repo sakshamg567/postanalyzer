09339170a2390a37fbe2ceb570ee758bc5d38edd
sakshamg567 <saksham060306@gmail.com>
2025-01-09 16:34:45 +0530

    ui updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,32 @@
+import { useRef } from "react";
 import AboutUs from "./components/AboutUs";
 function App() {
+  const homeRef = useRef(null)
+  const aboutUsRef = useRef(null)
+  // const homeRef = useRef(null)
+  // const homeRef = useRef(null)
 
+  const scrollToSection = (ref) => {
+    ref.current.scrollIntoView({behaviour: "smooth"})
+  } 
   return (
     <>
-      <div className="bg-gradient-to-br from-blue-200 via-blue-300  to-blue-200 min-h-screen flex items-center justify-center flex-col">
-        <div className="container flex items-center justify-center min-h-screen flex-col">
-          <span className="font-extrabold text-5xl max-w-2xl text-center relative -top-24 font-poppin ">
-            Unlock the power of Data, Drive smarter Social Engagement
-          </span>
-          <button className="self-center bg-blue-500 text-white min-w-32 min-h-11 rounded-full   hover:scale-125 hover:border-2 border-white absolute mt-32 transition-all duration-300 ease-in-out">Try Now !</button>
-          <section>
+      <div className="bg-gradient-to-br from-blue-200 via-blue-300  to-blue-200 min-h-screen flex items-center justify-center flex-col p-10">
+      <nav className="">
+            <ul className="flex flex-row top-0">
+              <li><button onClick={homeRef}>Home</button></li>
+              <li><button onClick={aboutUsRef}>About us</button></li>
+            </ul>
+      </nav>
+        <div className="container flex items-center justify-center min-h-screen flex-col scale-75 m-0">
+          
+          <section ref={homeRef} className="flex items-center justify-center flex-col">
+            <span className="font-extrabold text-5xl max-w-2xl text-center -top-40 font-poppin ">
+              Unlock the power of Data, Drive smarter Social Engagement
+            </span>
+            <button className="self-center bg-blue-500 text-white min-w-32 min-h-11 rounded-full hover:scale-125 hover:border-2 border-white mt-9 mb-12 transition-all duration-300 ease-in-out">Try Now !</button>
+          </section>
+          <section ref={aboutUsRef}>
             <AboutUs />
           </section>
         </div>
--- frontend/src/components/AboutUs.jsx
+++ frontend/src/components/AboutUs.jsx
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React, {useState, useEffect} from "react"
 
+const token = import.meta.env.VITE_GITHUB_TOKEN;
 
 function AboutUs() {
   const [profiles, setProfiles] = useState([]);
@@ -16,7 +17,11 @@ function AboutUs() {
       try {
         const profileData = await Promise.all(
           githubUsers.map(async (username) => {
-            const response = await fetch(`https://api.github.com/users/${username}`);
+            const response = await fetch(`https://api.github.com/users/${username}`, {
+              headers: {
+                Authorization: `Bearer ${token}`
+              }
+            });
             const data = await response.json();
             return data;
           })
@@ -31,16 +36,31 @@ function AboutUs() {
   }, []);
 
   return(
-    <div id="About-us">
-      <h1>About Us</h1>
-      <p>We are a team of Passionate Developers, Making products that eases the life of others</p>
-      <div id="profile-container" className="flex flex-row max-h-80 ">
+    <div id="About-us" className="flex flex-col items-center text-center scale-75 md:scale-90 lg:scale-100">
+      <h1 className="font-bold text-3xl mb-2">About Us</h1>
+      <p className="mb-12 text-lg">We are a team of Passionate Developers, Making products that eases the life of others</p>
+      <div id="profile-container" className="flex flex-row max-h-96 gap-5  ">
         {profiles.map((profile) => (
-          <div key={profile.id} id="profile-card">
-              <a href={profile.html_url} target="_blank" rel="noopener noreferrer">
-                <img src={profile.avatar_url} alt={profile.login} />
-                <span>{profile.name || profile.login}</span>
-                <p>{profile.bio || "No bio available"}</p>
+          <div 
+            key={profile.id}
+            id="profile-card"
+            className="border-white border-2 rounded-xl p-7 border-solid max-w-72 bg-slate-400 backdrop-blur-xl bg-opacity-15 text-clip h-80"  
+          >
+              <a 
+                href={profile.html_url} 
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex flex-col items-center justify-center text-center"  
+              >
+                <img 
+                  src={profile.avatar_url}
+                  alt={profile.login} 
+                  className="rounded-full max-w-20 mb-1"  
+                />
+                <span className="mb-5 font-semibold">
+                  {profile.name || profile.login}
+                </span>
+                <p className="text-sm overflow-hidden text-ellipsis max-h-24">{profile.bio || "No bio available"}</p>
               </a>
           </div>
         ))}
